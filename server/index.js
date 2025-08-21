aad812cfe77998e8a0473e4393557566daaebd88
yujie6369-1 <s1103130@taivs.tp.edu.tw>
2025-08-22 02:49:38 +0800

    加上 CORS 支援

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const app = express();
 const PORT = 3001;
 
+
+app.use(cors());
 app.use(express.json());
 
 // 註冊 API
